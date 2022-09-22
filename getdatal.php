7abe65b3fe1373fe7d709e6b003e8a6ee8191cf1
Yuranis Henriquez <yuranis@gmail.com>
2022-09-21 20:25:57 -0500

    Create getdatal.php

---
+++ getdatal.php
@@ -0,0 +1,18 @@
+<?php
+
+require(__DIR__.'/../../config.php');
+
+$learning_style_data = $DB->get_records("learning_style");
+
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=learning_style_data.csv');
+
+$data = "id,user,state,course,act_ref,sen_int,vis_vrb,seq_glo,created_at,updated_at\n";
+foreach($learning_style_data as $key => $value){
+    foreach($value as $k => $v){
+        $data .= "$v,";
+    }
+    $data .= "\n";
+}
+
+print($data);
