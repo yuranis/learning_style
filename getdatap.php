e52703dde59634a4184e8563b86df67fe21dff6e
Servidor Y <soporteti@utb.edu.co>
2022-09-22 09:08:25 -0500

    ajustes al estilo

--- getdatap.php
+++ getdatap.php
@@ -4,8 +4,8 @@ require(__DIR__.'/../../config.php');
 
 $learning_style_data = $DB->get_records("personality_test");
 
-#header('Content-Type: text/csv; charset=utf-8');
-#header('Content-Disposition: attachment; filename=personality_test.csv');
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=personality_test.csv');
 
 $data = "id,user,state,course,extraversion,introversion,sensing,intuition,thinking,feeling,judging,perceptive,created_at,updated_at\n";
 foreach($learning_style_data as $key => $value){
